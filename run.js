15cfe05958d4608cf3d7a781de33f5ed91861db1
k33g <ph.charriere@gmail.com>
2017-01-17 22:23:45 +0100

    ...

--- run.js
+++ run.js
@@ -4,7 +4,7 @@
 
 require('shelljs/global');
 
-[...Array(15).keys()].forEach(key => exec(
+[...Array(3).keys()].forEach(key => exec(
     `SERVICE_ID="gateway-${key}" PORT=${8081+key} MAPPED_PORT=${8081+key} HOST="localhost" REDIS_URL="redis://localhost:6379" node index.js`
   , (code, stdout, stderr) => {})
 )
