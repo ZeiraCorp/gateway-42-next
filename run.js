896d4da86054a8061d0a789f83f72179e5bca153
k33g <ph.charriere@gmail.com>
2017-01-17 21:54:47 +0100

    🐼 has pushed the initial version

---
+++ run.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+"use strict";
+
+require('shelljs/global');
+
+[...Array(15).keys()].forEach(key => exec(
+    `SERVICE_ID="gateway-${key}" PORT=${8081+key} MAPPED_PORT=${8081+key} HOST="localhost" REDIS_URL="redis://localhost:6379" node index.js`
+  , (code, stdout, stderr) => {})
+)
