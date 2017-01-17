b8c76269bc0b5c256b0ea319273353eea0d26047
Philippe Charrière <ph.charriere@gmail.com>
2017-01-17 22:35:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ let iam = () => {
         , INSTANCE_TYPE: process.env.INSTANCE_TYPE || "INSTANCE_TYPE"
         , COMMIT_ID: process.env.COMMIT_ID || "COMMIT_ID"
         , INSTANCE_NUMBER: process.env.INSTANCE_NUMBER || "INSTANCE_NUMBER"
-        , kind:"💧 🔥"
+        , kind:"🌧☀️🌈"
       }
   }));
 }
